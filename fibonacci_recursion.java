c11c4343acb24a73c54531487f4131eac694d261
prajwal mhaskar <33192878+PRAJWALMHASKAR@users.noreply.github.com>
2024-08-20 01:26:22 +0530

    Updated fibonacci_recursion.java
    updated execution of program, converted to recursion without memoization.

--- fibonacci_recursion.java
+++ fibonacci_recursion.java
@@ -6,32 +6,21 @@ public static void main (String[] args) {
 
 //0 1 1 2 3 5 8 13 21 34 55
 
-//wap to print fibonacci sequence of first 10 numbers
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter the number to print thhe fibonacci sequence");
-int i = sc.nextInt();
-
-System.out.println();
-
-fibonacci(i);
-
-}
-
-static int fibonacci (int seq) {
-	
-	int i=0;
-	int j=1;
-	int fib;
-	
-System.out.print("Fibonacci sequence is: "+i+" "+j+" ");
-	
-	for(seq=seq-2; seq>0; seq--) {
-		fib = i+j;
-		System.out.print(fib+" ");
-		i=j;
-		j=fib;
+	//wap to print fibonacci sequence of first 10 numbers
+	for(int j=0; j<=10; j++)
+	{
+		System.out.println(fib_rec(j));
 	}
-	return 0;
-}
 
+}
+	
+//Using recursion
+static int fib_rec(int n) {
+	if(n<=1)
+	{
+		return n;
+	}
+	return fib_rec(n-1)+fib_rec(n-2);
+}
+	
 }

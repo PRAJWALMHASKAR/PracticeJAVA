5e3b76f9bc3ea8977c6fe91d59b80628c9b289fe
prajwal mhaskar <33192878+PRAJWALMHASKAR@users.noreply.github.com>
2024-08-25 16:18:02 +0530

    Updated Reverse.java (interesting func-in-func concept)

--- Reverse.java
+++ Reverse.java
@@ -1,7 +1,9 @@
+//importing math library
 import java.util.*;
 
 public class Reverse {
 
+//function to reverse a number. This function returns another function.
 static int rev(int n) {
 
 int digits = (int)(Math.log10(n)+1);
@@ -9,6 +11,7 @@ return helper(n,digits);
 
 }
 
+//this function uses pow to add numbers  and it is main part of calculation of reverse number
 private static int helper(int n, int digits) {
 
 if(n%10 == n) {
@@ -16,6 +19,7 @@ return n;
 }
 
 int rem = n%10;
+//recurrence relation -- important
 return rem*(int)(Math.pow(10,digits-1)) + helper(n/10, digits-1);
 
 }
